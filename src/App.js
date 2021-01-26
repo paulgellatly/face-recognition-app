517c69490af6fdff20776a31c219623a997deda7
Paul G <paulgellatly@icloud.com>
2021-01-26 18:05:28 +0000

    added face recognition with box outline

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
 import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import './App.css';
+
+const app = new Clarifai.App({
+    apiKey: '04b8274fae9d43ce920b9486416a8327',
+});
 
 const particlesOptions = {
     particles: {
@@ -18,17 +24,74 @@ const particlesOptions = {
     },
 };
 
-function App() {
-    return (
-        <div className="App">
-            <Particles className="particles" params={particlesOptions} />
-            <Navigation />
-            <Logo />
-            <Rank />
-            <ImageLinkForm />
-            {/* <FaceRecognotion /> */}
-        </div>
-    );
+class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            input: '',
+            imageUrl: '',
+            box: {},
+        };
+    }
+
+    calculateFaceLocation = (data) => {
+        const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+        const image = document.getElementById('inputimage');
+        const width = Number(image.width);
+        const height = Number(image.height);
+        return {
+            leftCol: clarifaiFace.left_col * width,
+            topRow: clarifaiFace.top_row * height,
+            rightCol: width - clarifaiFace.right_col * width,
+            bottomRow: height - clarifaiFace.bottom_row * height,
+        };
+    };
+
+    displayFaceBox = (box) => {
+        console.log(box);
+        this.setState({ box: box });
+    };
+
+    onInputChange = (event) => {
+        this.setState({ input: event.target.value });
+    };
+
+    onButtonSubmit = () => {
+        this.setState({ imageUrl: this.state.input });
+        app.models
+            .initModel({
+                id: Clarifai.FACE_DETECT_MODEL,
+            })
+            .then((faceDetectModel) => {
+                return faceDetectModel.predict(this.state.input);
+            })
+            .then((response) => this.displayFaceBox(this.calculateFaceLocation(response)))
+            .catch((error) => console.log(error));
+        // app.models.predict('c0c0ac362b03416da06ab3fa36fb58e3', this.state.input).then(
+        //     function (response) {
+        //         console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
+        //     },
+        //     function (err) {
+        //         //there was an error
+        //     }
+        // );
+    };
+
+    render() {
+        return (
+            <div className="App">
+                <Particles className="particles" params={particlesOptions} />
+                <Navigation />
+                <Logo />
+                <Rank />
+                <ImageLinkForm
+                    onInputChange={this.onInputChange}
+                    onButtonSubmit={this.onButtonSubmit}
+                />
+                <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl} />
+            </div>
+        );
+    }
 }
 
 export default App;

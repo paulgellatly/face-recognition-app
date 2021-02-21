6727d49a7d3305e50243842e48574eb351056d09
Paul G <paulgellatly@icloud.com>
2021-02-21 21:14:29 +0000

    linked front end to back end and added functionality

--- src/App.js
+++ src/App.js
@@ -35,8 +35,27 @@ class App extends Component {
             box: {},
             route: 'signin',
             isSignedIn: false,
+            user: {
+                id: '',
+                name: '',
+                email: '',
+                entries: 0,
+                joined: '',
+            },
         };
     }
+
+    loadUser = (data) => {
+        this.setState({
+            user: {
+                id: data.id,
+                name: data.name,
+                email: data.email,
+                entries: data.entries,
+                joined: data.joined,
+            },
+        });
+    };
 
     calculateFaceLocation = (data) => {
         const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
@@ -69,7 +88,22 @@ class App extends Component {
             .then((faceDetectModel) => {
                 return faceDetectModel.predict(this.state.input);
             })
-            .then((response) => this.displayFaceBox(this.calculateFaceLocation(response)))
+            .then((response) => {
+                if (response) {
+                    fetch('http://localhost:3000/image', {
+                        method: 'put',
+                        headers: { 'Content-Type': 'application/json' },
+                        body: JSON.stringify({
+                            id: this.state.user.id,
+                        }),
+                    })
+                        .then((response) => response.json())
+                        .then((count) => {
+                            this.setState(Object.assign(this.state.user, { entries: count }));
+                        });
+                }
+                this.displayFaceBox(this.calculateFaceLocation(response));
+            })
             .catch((error) => console.log(error));
         // app.models.predict('c0c0ac362b03416da06ab3fa36fb58e3', this.state.input).then(
         //     function (response) {
@@ -99,7 +133,7 @@ class App extends Component {
                 {route === 'home' ? (
                     <div>
                         <Logo />
-                        <Rank />
+                        <Rank name={this.state.user.name} entries={this.state.user.entries} />
                         <ImageLinkForm
                             onInputChange={this.onInputChange}
                             onButtonSubmit={this.onButtonSubmit}
@@ -107,9 +141,9 @@ class App extends Component {
                         <FaceRecognition box={box} imageUrl={imageUrl} />
                     </div>
                 ) : route === 'signin' ? (
-                    <Signin onRouteChange={this.onRouteChange} />
+                    <Signin loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
                 ) : (
-                    <Register onRouteChange={this.onRouteChange} />
+                    <Register loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
                 )}
             </div>
         );
